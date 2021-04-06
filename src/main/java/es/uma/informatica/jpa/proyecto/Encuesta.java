13a5bc20f8bc7e9681e9508aaf496c5369aeface
AbdeUMA <akabd953@uma.es>
2021-04-06 19:36:28 +0200

    Arreglando error de clase

--- src/main/java/es/uma/informatica/jpa/proyecto/Encuesta.java
+++ src/main/java/es/uma/informatica/jpa/proyecto/Encuesta.java
@@ -28,7 +28,7 @@ public class Encuesta implements Serializable {
 	private Expedientes expediente;
 	
 	//Lista para la relación con grupos por asignatura
-	private List<Grupos_por_asignatura> asignaturas;
+	private List<Grupos_Por_Asignatura> asignaturas;
 	
 	public Encuesta() {
 		super();
