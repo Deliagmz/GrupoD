fe1d2a8f3f22c700f85628d8256e360556fe4428
GuiJimenez <guijimenez@uma.es>
2021-04-06 11:40:47 +0200

    hashcode, equals y toString de Expedientes

--- src/main/java/es/uma/informatica/jpa/proyecto/Expedientes.java
+++ src/main/java/es/uma/informatica/jpa/proyecto/Expedientes.java
@@ -45,5 +45,45 @@ public class Expedientes implements Serializable {
 	public void setNota_Media_Provisional(Double Nota_Media_Provisional) {
 		this.Nota_Media_Provisional = Nota_Media_Provisional;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Activo == null) ? 0 : Activo.hashCode());
+		result = prime * result + ((Nota_Media_Provisional == null) ? 0 : Nota_Media_Provisional.hashCode());
+		result = prime * result + ((Num_Expediente == null) ? 0 : Num_Expediente.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Expedientes other = (Expedientes) obj;
+		if (Activo == null) {
+			if (other.Activo != null)
+				return false;
+		} else if (!Activo.equals(other.Activo))
+			return false;
+		if (Nota_Media_Provisional == null) {
+			if (other.Nota_Media_Provisional != null)
+				return false;
+		} else if (!Nota_Media_Provisional.equals(other.Nota_Media_Provisional))
+			return false;
+		if (Num_Expediente == null) {
+			if (other.Num_Expediente != null)
+				return false;
+		} else if (!Num_Expediente.equals(other.Num_Expediente))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Expedientes [Num_Expediente=" + Num_Expediente + ", Activo=" + Activo + ", Nota_Media_Provisional="
+				+ Nota_Media_Provisional + "]";
+	}
    
 }
