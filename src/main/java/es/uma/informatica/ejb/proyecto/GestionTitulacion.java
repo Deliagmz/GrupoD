1af565b8be2024fc8d54754e11bdea6d7a23c273
Deliagmz <delia3700@uma.es>
2021-04-19 18:59:28 +0200

    EJB titulacion y matricula

---
+++ src/main/java/es/uma/informatica/ejb/proyecto/GestionTitulacion.java
@@ -0,0 +1,17 @@
+package es.uma.informatica.ejb.proyecto;
+import javax.ejb.Local;
+
+import es.uma.informatica.jpa.proyecto.Titulacion;
+
+
+
+@Local
+public interface GestionTitulacion {
+	
+	public Titulacion obtenerTitulacion(Integer codigo) throws SecretariaException;
+	
+	public void eliminarTitulacion(Integer codigo) throws SecretariaException;
+	
+	public void actualizarTitulacion(Integer codigo,Titulacion titulacion) throws SecretariaException;
+	
+}
