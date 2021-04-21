85d1ca1d4757bd6d1ad436ca212efcb0ed524692
GuiJimenez <guijimenez@uma.es>
2021-04-21 20:28:21 +0200

    inserta datos correctos excepto: titulacion, matricula y encuesta

--- src/main/java/es/uma/informatica/jpa/proyecto/Main.java
+++ src/main/java/es/uma/informatica/jpa/proyecto/Main.java
@@ -1,5 +1,8 @@
 package es.uma.informatica.jpa.proyecto;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -8,12 +11,68 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Secretaria");
-        EntityManager em = emf.createEntityManager();
-        em.close();
-        emf.close();
-        
-
-    }
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("Secretaria");
+		EntityManager em = emf.createEntityManager();
+		
+	
+		em.getTransaction().begin();
+		
+		Alumno alumno = new Alumno();
+		alumno.setNombre("Paco");
+		alumno.setID(213456789L);
+		alumno.setApellido1("luna");
+		alumno.setApellido2("sol");
+		alumno.setDNI("789456123X");
+		alumno.setEmail_institucional("paco@uma.es");
+		
+		em.persist(alumno);
+		
+		Asignaturas asignatura = new Asignaturas();
+		asignatura.setReferencia(12345);
+		asignatura.setCodigo(1111111);
+		asignatura.setCreditos(6);
+		asignatura.setOfertada(true);
+		asignatura.setNombre("Matematica discreta");
+		
+		em.persist(asignatura);
+		
+//		Titulacion titulacion = new Titulacion();
+//		titulacion.setCodigo(1234);
+//		titulacion.setCreditos(240);
+//		titulacion.setNombre("Informatica");
+//		
+//		em.persist(titulacion);
+		
+		Expedientes expedientes = new Expedientes();
+		expedientes.setNum_Expediente(123456789L);
+		expedientes.setNota_Media_Provisional(5.0);
+		
+		em.persist(expedientes);
+		
+//		Encuesta encuesta = new Encuesta();
+//		
+//		Timestamp t = java.sql.Timestamp.valueOf("2021-09-23 10:10:10.0");
+//		encuesta.setFecha_de_envio(t);
+//		
+//		em.persist(encuesta);
+		
+//		Matricula matricula = new Matricula();
+//		matricula.setCurso_academico("1");
+//		matricula.setEstado("activo");
+//		Date fecha = java.sql.Date.valueOf("2021-09-23");
+//		matricula.setFecha_de_matricula(fecha);
+//
+//		
+//		
+//		em.persist(matricula);
+		
+		
+		
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
 
 }
